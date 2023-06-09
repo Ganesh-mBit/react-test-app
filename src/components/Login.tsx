40f90e5f0ef88ed1a4e88aa185f4ca1e1e0fa0fc
Ganesh M <ganeshm@Ganeshs-MacBook-Air.local>
2023-06-09 16:17:54 +0530

    Login & Signup testing setup done.

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,95 +1,35 @@
-import React, { type ChangeEvent, type FormEvent, useState } from 'react';
-import './Login.css';
+import React from 'react';
+import { Login, login } from 'react-package-bt';
+import { useDispatch } from 'react-redux';
 
-interface Field {
-  name: string
-  label: string
-  type: string
-  required: boolean
-}
+const LoginMain = (): JSX.Element => {
+  const dispatch = useDispatch();
 
-interface LoginProps {
-  title: string
-  logoUrl: string
-  fields: Field[]
-  error?: string
-}
-
-const Login: React.FC<LoginProps> = ({ title, logoUrl, fields, error }) => {
-  const [formData, setFormData] = useState<Record<string, string>>({});
-  const [errors, setErrors] = useState<Record<string, string>>({});
-
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value
-    }));
-    setErrors({});
-  };
-
-  const handleSubmit = (e: FormEvent): void => {
-    e.preventDefault();
-    // Perform form validation
-    const validationErrors: Record<string, string> = validateForm();
-    setErrors(validationErrors);
-    if (Object.keys(validationErrors).length === 0) {
-      // Form is valid, perform further actions
-      console.log('Form submitted:', formData);
+  const onSubmit = (data: any): void => {
+    if (data) {
+      dispatch(login('https://api.gamestoppedout.com/auth/v1.0/login', data, () => { alert('Signup successful'); }, () => { alert('Opps Something went wrong!'); }));
     }
-  };
-
-  const validateForm = (): Record<string, string> => {
-    const validationErrors: Record<string, string> = {};
-    for (const field of fields) {
-      if (field.required && !formData[field.name]) {
-        validationErrors[field.name] = `${field.label} is required.`;
-      }
-      // if (field.type === 'email' && formData[field.name]) {
-      //   const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(
-      //     formData[field.name]
-      //   );
-      //   if (!isValidEmail) {
-      //     validationErrors[field.name] = 'Invalid email address.';
-      //   }
-      // }
-      // Add additional validations as needed for different field types
-    }
-    return validationErrors;
   };
 
   return (
-    <div className="login-form">
-      <div className="top-section">
-        <img src={logoUrl} alt="Logo" className="logo" />
-        <h2 className="form-title">{title}</h2>
-      </div>
-      {error && !Object.keys(errors).length && (
-        <div className="error-message">{error}</div>
-      )}
-      <form onSubmit={handleSubmit}>
-        {fields.map((field) => (
-          <div key={field.name} className="form-field-container">
-            <div className="form-field">
-              <label htmlFor={field.name}>{field.label}</label>
-              <input
-                type={field.type}
-                id={field.name}
-                name={field.name}
-                value={formData[field.name] || ''}
-                onChange={handleInputChange}
-                className={errors[field.name] ? 'input-error' : ''}
-              />
-              {errors[field.name] && (
-                <div className="error-message">{errors[field.name]}</div>
-              )}
-            </div>
-          </div>
-        ))}
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Login
+      title='Log In'
+      logoUrl='https://images.pexels.com/photos/2835170/pexels-photo-2835170.jpeg?auto=compress&cs=tinysrgb&w=180&h=250&dpr=2'
+      formSx={{ width: { xs: '90%', md: '40%' }, display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 4 }}
+      fields={[
+        { name: 'username', label: 'Username', type: 'text', required: true, sxField: { grid: { xs: 12, md: 12 } } },
+        { name: 'password', label: 'Password', type: 'password', required: true, sxField: { grid: { xs: 12, md: 12 } } }
+      ]}
+      showSocialLogin={true}
+      showCreateAccount={true}
+      showForgotPassword={false}
+      socialProviders={['facebook', 'google', 'twitter']}
+      onSocialLogin={() => { console.log('Social Login'); }}
+      onForgotPassword={() => { console.log('Forgot Password'); }}
+      onCreateAccount={() => { console.log('Create New Account'); }}
+      onSubmit={onSubmit}
+    />
   );
 };
 
-export default Login;
+export default LoginMain;
